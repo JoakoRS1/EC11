f0366067865dee37a72e1bdef5378a7854dfc430
Gabriel <sam.perezf@hotmail.com>
2022-05-01 15:45:26 -0500

    importe Carta a MainActivity (creo)

--- app/src/main/java/com/example/ec11/MainActivity.kt
+++ app/src/main/java/com/example/ec11/MainActivity.kt
@@ -6,18 +6,19 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ListView
 import android.widget.TextView
-import com.example.ec11.Views.Carta
+import com.example.ec11.Views.*
 import org.w3c.dom.Text
 import java.util.*
 import java.util.jar.Attributes
 
 class MainActivity : AppCompatActivity() {
 
-    class Carta_Clase (val numero: Int, val palo: String){}
-    private var Mazo = mutableListOf<Carta_Clase>()
-    private var SubMazo1 = mutableListOf<Carta_Clase>()
-    private var SubMazo2 = mutableListOf<Carta_Clase>()
-    private var SubMazo3 = mutableListOf<Carta_Clase>()
+    //class Carta_Clase (val numero: Int, val palo: String){}
+    var Carta: Carta? = null
+    private var Mazo = mutableListOf<Carta>()
+    private var SubMazo1 = mutableListOf<Carta>()
+    private var SubMazo2 = mutableListOf<Carta>()
+    private var SubMazo3 = mutableListOf<Carta>()
     private var CartasenMazo: Int=52
     private var J1CartasSubMazo: Int=0
     private var J2CartasSubMazo: Int=0
@@ -40,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         for (i in arrayOf("corazon","espada","trebol","diamante")){
             for (j in 1..13) {
-                Mazo.add(Carta_Clase(j,i))
+                Mazo.add(Carta(this,j,i))
             }
         }
         shuffle(Mazo,52)
@@ -60,24 +61,24 @@ class MainActivity : AppCompatActivity() {
         TvJ3CartasMazo.text=J3CartasSubMazo.toString()
     }
 
-    fun shuffle(cards: MutableList<Carta_Clase>, n: Int) {
+    fun shuffle(cards: MutableList<Carta>, n: Int) {
         var card = cards
         val rand = Random()
         for (i in 0 until n) {
             val r: Int = i + rand.nextInt(52 - i)
             //swapping the elements
-            val temp: Carta_Clase = card[r]
+            val temp: Carta = card[r]
             card[r] = card[i]
             card[i] = temp
         }
     }
     fun crearVistaCarta(CardView: Carta){
-        CardView.number=Mazo[3].numero.toString()
+        CardView.number=Mazo[3].number
         CardView.palo=Mazo[3].palo
     }
 
 
-    fun robarCarta (): Carta_Clase{
+    fun robarCarta (): Carta{
         //-1 Mazo principal
         var carta_repartida=Mazo.removeAt(0)
         CartasenMazo-=1
