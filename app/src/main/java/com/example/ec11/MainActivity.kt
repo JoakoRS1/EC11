16aa967946af08319a8d6734b3dd5deeafba337c
Gabriel <sam.perezf@hotmail.com>
2022-05-03 20:47:46 -0500

    fucniona robar (en boton)

--- app/src/main/java/com/example/ec11/MainActivity.kt
+++ app/src/main/java/com/example/ec11/MainActivity.kt
@@ -274,7 +274,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         InicializaJuego()
         DuranteJuego()
     }
